f8a48535a38314109513c4d59117a0a802ac20d9
Gabriel_Setznagl <Gabrielembraer3@gmail.com>
2024-02-07 02:07:43 -0300

    .

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -53,26 +53,3 @@ function listaTela(largura , altura){
    else if (prop > 2.8 && largura > 900){
        return 16;}
    else return 6}
-
-/*/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////// Vanila PokeAPI ////////////////////////////////////////////////////////////////////
-
-const pokeAPI = {};
-
-pokeAPI.getPokemonDetail = (pokemon) => {return fetch(pokemon.url).then((response) => response.json())}
-
-pokeAPI.getPokemons = (offset = 0 , limit = 6) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-    return fetch(url)
-        // o primeiro .then recebe a promisse que veio via fetch e o segundo .then recebe o retorno do primeiro já em objeto JSON e o terceiro pega somente o array dos resultados .results
-        .then((response) => response.json())
-        .then((responseToJSON) => responseToJSON.results) // results é o nome do array contendo os pokemons 
-        .then((responseResults)=> responseResults.map(pokeAPI.getPokemonDetail)) //usa a função map nos indices para listar um array dos detalhes dos pokemons
-        .then((detailResponses)=> Promise.all(detailResponses))
-        .then((returnResponses)=> returnResponses)
-        ///////////////////////////////////////////////////////////////////////////////////////////////////
-        .catch((error) =>console.error(error))
-        .finally(() => { console.log("Loading! response received")});
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////*/
